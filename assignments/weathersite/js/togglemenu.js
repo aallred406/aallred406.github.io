80f81e6ff48bb84152e67f6a237b049d13fd2486
aallred406 <allredfamily04@gmail.com>
2019-03-22 12:06:40 -0600

    update

--- assignments/weathersite/js/togglemenu.js
+++
@@ -1,7 +0,0 @@
-//function toggleMenu(){
-  //document.getElementById("primaryNav").classList.toggle("hide");
-//}
-
-function toggleMenu() {
-	document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
-}
--- assignments/weathersite/js/windchill.js
+++
@@ -1,5 +0,0 @@
-  let tempF = parseFloat(document.getElementById("tempF").innerHTML);
-  let speed = parseFloat(document.getElementById("wind").innerHTML);
-  let factor = ((35.74 + 0.6215 * tempF) - (35.75 * Math.pow(speed,0.16)) + (0.4275 * tempF * Math.pow(speed,0.16))).toFixed(2);  
-  document.getElementById("windchill").innerHTML = factor + "&deg;";
-
